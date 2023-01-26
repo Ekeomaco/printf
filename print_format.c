ecb1da6f29840f90cd1e2515ef2f675e34bb66b6
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-26 11:53:53 +0000

    Added tasks

--- print_format.c
+++ print_format.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _print_format - Prints a format
  * @format: The format to prints
  * @args: A list of variadic arguments
- *
  * Return: The length of the format
  */
 int _print_format(const char *format, va_list args)
@@ -30,75 +28,8 @@ int _print_format(const char *format, va_list args)
 				count += _print_spec(format[i], args);
 		}
 		else
-		{
 			count += _write(format[i]);
-		}
 		i++;
 	}
 	return (count);
-}
-
-/**
- * _print_spec - Prints a valid specifier
- * @format: The specifier to prints
- * @args: A list of variadic arguments
- *
- * Return: The length of the specifier
- */
-int _print_spec(char format, va_list args)
-{
-	int i = 0, length = 0;
-	spc_dt _types[] = {
-		{"c", _print_a_char},
-		{"s", _print_a_string},
-		{"d", _print_a_integer},
-		{"i", _print_a_integer},
-		{"b", _print_int_binary},
-		{NULL, NULL}};
-	while (_types[i].specifier)
-	{
-		if (*_types[i].specifier == format)
-			length = _types[i].f(args);
-		i++;
-	}
-	return (length);
-}
-/**
- * _print_invalid_spec - Prints a invalid specifier
- * @prev_format: The previous specifier of actual specifier
- * @format: The specifier to prints
- * @count: The current count before prints invalid specifiers
- *
- * Return: The current count after prints invalid specifiers
- */
-int _print_invalid_spec(char prev_format, char format, int count)
-{
-	count += _write('%');
-	if (prev_format == ' ')
-	{
-		count += _write(' ');
-		count += _write(format);
-	}
-	else
-		count += _write(format);
-	return (count);
-}
-/**
- * _validate_char - validate the type
- * @_type: character to be comparate
- *
- * Return: 1 if char is equal to a type
- */
-int _validate_char(char _type)
-{
-	char _types[] = {'c', 's', 'd', 'i', 'b', '%'};
-	int i = 0;
-
-	while (_types[i])
-	{
-		if (_types[i] == _type)
-			return (1);
-		i++;
-	}
-	return (0);
 }
